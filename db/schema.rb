ab7e3af2bc1af041ab9971b0abf4751fcca8eb0e
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-22 17:05:03 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -19,10 +19,8 @@ ActiveRecord::Schema.define(version: 20140212205318) do
   end
 
   create_table "rides", force: :cascade do |t|
-    t.integer  "taxis_id"
-    t.integer  "passenger_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "taxis", force: :cascade do |t|
